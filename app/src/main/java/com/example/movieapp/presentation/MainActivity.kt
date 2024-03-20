4f96f9c26051a297b23093f1555558441616e63f
murat-dayan <muratdayan359@gmail.com>
2024-03-21 02:38:19 +0300

    Package Structure Created

--- app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.movieapp
+package com.example.movieapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.movieapp.ui.theme.MovieAppTheme
+import com.example.movieapp.presentation.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
