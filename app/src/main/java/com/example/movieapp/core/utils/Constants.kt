446dcff848ecf1b95673215b0177c1f856a8a87f
muratdayan <muratdayan359@gmail.com>
2024-04-13 20:38:47 +0300

    pulling cast datas from api for series and movies

---
+++ app/src/main/java/com/example/movieapp/core/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.movieapp.core.utils
+
+import com.example.movieapp.BuildConfig
+
+object Constants {
+
+    const val API_KEY = BuildConfig.API_KEY
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+}
